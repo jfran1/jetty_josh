abfd3bece210b24f96ec080401f5491efd2fd87d
Josh Frandsen <joshua.frandsen@berkeley.edu>
2017-06-21 19:57:55 -0700

    fix plot

--- plot13TeV.C
+++ plot13TeV.C
@@ -2,86 +2,55 @@
 void plot13TeV()
 {
 
-	TFile *f1= TFile::Open("/Users/joshfrandsen/test/jetty/output/minBias13TeV.root");
+	TFile *f1= TFile::Open("/Users/joshfrandsen/test/jetty/output/13TeV.root");
 	if (f1 ==0) cout<< "ERROR! File 1 didn't open" << endl;
 	else cout << "File 1 Opened!" << endl;
 
-	TFile *f2 = TFile::Open("/Users/joshfrandsen/test/jetty/output/inel13TeV.root"); 
-	if (f2==0) cout<< "ERROR! File 2 didn't open" << endl;
-	else cout << "File2 Opened!" << endl;
-	
-
-	TFile *f3 = TFile::Open("/Users/joshfrandsen/test/jetty/output/13TeVSum.root");
-	if (f3==0) cout<< "ERROR! File 3 didn't open" << endl;
-	else cout << "File 3 Opened!" << endl;
-	
-
-	TFile *f4 = TFile::Open("/Users/joshfrandsen/test/jetty/output/13TeVAliceResults.root");
-	if (f4==0) cout<< "ERROR! File 4 didn't open" << endl;
+	TFile *f2 = TFile::Open("/Users/joshfrandsen/test/jetty/output/13TeVAliceResults.root");
+	if (f2==0) cout<< "ERROR! File 4 didn't open" << endl;
 	else cout << "File 4 Opened!" << endl;
-	f4->cd("Table 2");
+	f2->cd("Table 2");
 	
 
 	TH1F *aliceResults = (TH1F*)gDirectory->Get("Hist1D_y1");
-	TFile *files[4] = {f1,f2,f3,f4}; 
-	TH1F *data[2];
-	TH1F *minBias = (TH1F*)f1->Get("hpTRaw"); // not scaled 
-	TH1F *inel = (TH1F*)f2->Get("hpTRaw"); // not scaled
-	TH1F *hard = (TH1F*)f3->Get("hpTRaw"); // slcaled but not by 1/pT
-	TF1 *line = new TF1("line", "pol1", 0, 100); // line for 1/pT scaling
-	line->SetParameter(0,0);
-	line->SetParameter(1,1);
+	TH1F *TeV13 = (TH1F*)f1->Get("hpTRaw"); // not scaled 
+	TH1F *data = (TH1F*)f1->Get("data");
 
-	double sigma[2];
-	double binWidth[2];
-	double weightSum[2];
+
+	double sigma = data->GetBinContent(1);
+	double weightSum = data->GetBinContent(2);
+	double binWidth = data->GetBinContent(3);
 	double dEta = 0.8;
 
-	// 0=minbias, 1=inel 
-	for (int i=0; i < 2;i++)
-	{
-		data[i] = (TH1F*)files[i]->Get("data");
-		sigma[i]= data[i]->GetBinContent(1);
-		binWidth[i] = data[i]->GetBinContent(3);
-		weightSum[i] = data[i]->GetBinContent(2);
-		cout << "cross section " << i << ": " << sigma[i] << endl;
-		cout << "weightSum " << i << ": " << weightSum[i] << endl;
-	}
+	cout << "cross section: "<< sigma << endl;
+	cout << "weightSum: " << weightSum << endl;
+	cout << "Bin Width: " << binWidth << endl;
 
 
 
 	// scaling 
-	minBias->Scale( sigma[0] / ( 2*TMath::Pi() * weightSum[0] * binWidth[0] * 2 * TMath::Pi() *2*dEta) );
-	minBias->Divide(line);
-	inel->Scale( sigma[1] / ( 2*TMath::Pi() * weightSum[1] * binWidth[1] * 2 * TMath::Pi() *2*dEta ));
-	inel->Divide(line);
-	hard->Divide(line);
+	TeV13->Scale( 1 / ( 2*TMath::Pi() * weightSum * binWidth * 2 * TMath::Pi() *2*dEta) );
+	TH1F *copyTeV13 = (TH1F*)TeV13->Clone();
+	copyTeV13->Scale(5);
 
 	// Drawing all three same plot
 	TCanvas *c1 = new TCanvas("c1");
 	c1->SetLogy();
-	minBias->SetYTitle("1/(2#pip_{T})(d#sigma^{2}/(d#etadp_{T}) [mb GeV^{-2}]");
-	minBias->Draw("hist");
-	inel->SetLineColor(kRed);
-	inel->SetYTitle("1/(2#pip_{T})(d#sigma^{2}/(d#etadp_{T}) [mb GeV^{-2}]");
-	inel->Draw("hist same");
-	hard->SetLineColor(kBlack);
-	hard->SetYTitle("1/(2#pip_{T})(d#sigma^{2}/(d#etadp_{T}) [mb GeV^{-2}]");
-	hard->Draw("same hist");
-	aliceResults->SetLineColor(kGreen);
+	TeV13->SetYTitle("1/(2#pip_{T})(dN^{2}/(d#etadp_{T}) [mb GeV^{-2}]");
+	TeV13->Draw("hist");
+	aliceResults->SetLineColor(kRed+4);
 	aliceResults->Draw("hist same");
+	copyTeV13->SetLineColor(kMagenta);
+	copyTeV13->Draw("hist same");
 
 	TLegend *leg = new TLegend();
-	leg->AddEntry(minBias, "Min Bias", "L");
-	leg->AddEntry(inel, "Inelastic", "L");
-	leg->AddEntry(hard, "Hard", "L");
-	leg->AddEntry(aliceResults, "Alice", "L");
+	leg->AddEntry(TeV13, "Pythia8 13 [TeV]", "L");
+	leg->AddEntry(aliceResults, "Alice 13 [TeV]", "L");
+	leg->AddEntry(copyTeV13, "Pythia8 Plot Scaled by 5", "L");
 	leg->Draw("same");
 
 
-	inel->SetTitle("Inelastic");
-	minBias->SetTitle("Min Bias");
-	hard->SetTitle("Hard (dif pT_Hat bins)");
+	TeV13->SetTitle("Pythia8 vs. Alice 13 TeV p_{T} Distribution");
 
 
 
@@ -89,9 +58,7 @@ void plot13TeV()
 TFile *fout = TFile::Open("13TeVPlots.root" , "RECREATE");
 fout->cd();
 
-inel->Write("inel");
-minBias->Write("minBias");
-hard-> Write("hard");
+TeV13->Write("TeV13");
 aliceResults->Write("aliceResults");
 
 fout->Close();	
